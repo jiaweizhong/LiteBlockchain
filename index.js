6e99b0d162431374b26af92a7fc3899111545e4d
Jiawei Zhong <jiawei@jiawei-PC>
2019-02-18 22:17:01 +0000

    Crypto v3

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 const bodyParser = require('body-parser');
 const express = require('express');
 const Blockchain = require('./blockchain');
-
+const PubSub = require('./pubsub');
 
 // express object to get request
 const app = express();
 const blockchain = new Blockchain();
+const pubsub = new PubSub({blockchain});
+
+setTimeout(() => pubsub.broadcastChain(), 1000);
 
 app.use(bodyParser.json());
 // get response
@@ -18,9 +21,19 @@ app.post('/api/mine', (req, res) => {
     // get data from user req
     const {data} = req.body;
     blockchain.addBlock({data});
+    // broadcast chain when new block is mined
+    pubsub.broadcastChain();
+
     res.redirect('/api/blocks');
 });
 
-const PORT = 3000;
+const DEFAULT_PORT = 3000;
+let PEER_PORT;
+// generate random peer port starting from 3000 - 4000
+if(process.env.GENERATE_PEER_PORT === "true"){
+    PEER_PORT = DEFAULT_PORT + Math.ceil(Math.random() * 1000);
+}
+
+const PORT = PEER_PORT || DEFAULT_PORT;
 // listen to local host 3000
-app.listen(PORT, (PORT) => {`listening at localhost:${PORT}`});
+app.listen(PORT, () => console.log(`listening at localhost:${PORT}`));
---
+++ pubsub.js
@@ -0,0 +1,49 @@
+const redis = require('redis');
+
+const CHANNELS = {
+    TEST: 'TEST',
+    BLOCKCHAIN: 'BLOCKCHAIN'
+}
+
+class PubSub{
+    constructor({blockchain}){
+        this.blockchain = blockchain;
+        
+        this.publisher = redis.createClient();
+        this.subscriber = redis.createClient();
+
+        this.subscribeToChannel();
+
+        this.subscriber.on('message',
+            (channel, message) => this.handelMessage(channel, message)
+        );
+    }
+    // replace the blockchain if receive a valid blockchain
+    handelMessage(channel, message){    
+        console.log(`Message received. Channel: ${channel}. Message: ${message}.`);
+        const parseMessage = JSON.parse(message); // parse blockchain info
+        if(channel=== CHANNELS.BLOCKCHAIN){
+            this.blockchain.replaceChain(parseMessage);
+        }
+    }
+
+    subscribeToChannel(){
+        // subscribe all the channels
+        Object.values(CHANNELS).forEach(channel => {
+            this.subscriber.subscribe(channel);
+        });
+    }
+    // publish message
+    publish({channel, message}){
+        this.publisher.publish(channel, message);
+    }
+    // blockchain should broadcast everytime a block is mined
+    broadcastChain(){
+        this.publish({
+            channel:  CHANNELS.BLOCKCHAIN,
+            message: JSON.stringify(this.blockchain.chain)
+        });
+    }
+}
+
+module.exports = PubSub;
